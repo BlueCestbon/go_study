89f6adef3e3f9f2e3fa52253ddd804e93d9f2f2d
xiaowei <1437591651@qq.com>
2024-12-05 22:17:29 +0800

    add:客户端实现建立连接

---
+++ base/IM-System/client/client.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net"
+)
+
+type Client struct {
+	ServerIp   string
+	ServerPort int
+	Name       string
+	Conn       net.Conn
+}
+
+func NewClient(serverIp string, serverPort int) *Client {
+	// 创建客户端对象
+	client := &Client{
+		ServerIp:   serverIp,
+		ServerPort: serverPort,
+	}
+	// 连接到server
+	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", serverIp, serverPort))
+	if err != nil {
+		fmt.Println("net.Dial err, ", err)
+		return nil
+	}
+
+	client.Conn = conn
+
+	// 返回对象
+	return client
+}
+
+var serverIp string
+var serverPort int
+
+func init() {
+	flag.StringVar(&serverIp, "ip", "127.0.0.1", "设置服务器IP地址(默认127.0.0.1)")
+	flag.IntVar(&serverPort, "port", 8888, "设置服务器端口(默认8888)")
+}
+
+func main() {
+	flag.Parse()
+	client := NewClient(serverIp, serverPort)
+	if client == nil {
+		fmt.Println(">>>>>连接失败...")
+		return
+	}
+
+	fmt.Println(">>>>>连接成功...")
+
+	// 阻塞
+	for {
+	}
+}
